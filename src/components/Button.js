245ca8cad30c1a15697eeba176f8c79643341191
parkhabit <parkhabit@gmail.com>
2018-01-23 18:06:13 +0000

    included a stop button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,21 +2,27 @@ import React from "react";
 
 export class Button extends React.Component {
   state = {
-    running: false,
-    health: 100
+    health: 100,
+    interval: null
   };
 
   healthDecrement = () => {
-    this.setState({ running: true });
-    if (this.state.health >= 0) {
-      setInterval(() => {
+    if (this.state.health > 0) {
+      const interval = setInterval(() => {
         this.setState({ health: this.state.health - 10 });
       }, 1000);
+      this.setState({ interval });
     }
+    stop();
   };
 
   healthIncrement = () => {
     this.setState({ health: this.state.health + 1 });
+  };
+
+  stop = () => {
+    clearInterval(this.state.interval);
+    this.setState({ health: 100 });
   };
 
   render() {
@@ -25,6 +31,7 @@ export class Button extends React.Component {
         <p>{this.state.health}</p>
         <button onClick={this.healthDecrement}>Start</button>
         <button onClick={this.healthIncrement}>Add health</button>
+        <button onClick={this.stop}>Stop</button>
       </div>
     );
   }
